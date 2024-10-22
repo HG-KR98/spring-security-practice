4c66fc691c19d20ea2920f2070274b97bcf2373a
김헌규 <khg6436@naver.com>
2024-10-22 23:11:03 +0900

    ãwebconfig 작성

---
+++ src/main/java/com/example/securitypractice/service/UserDetailService.java
@@ -0,0 +1,21 @@
+package com.example.securitypractice.service;
+
+import com.example.securitypractice.entity.User;
+import com.example.securitypractice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // 사용자 이름(email)으로 사용자의 정보를 가져오는 메서드
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
